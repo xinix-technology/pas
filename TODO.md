39a262bf55b7849c66afdf85b66aa2abb2168855
Ganesha <reekoheek@gmail.com>
2015-02-20 12:56:26 +0700

    various updates

---
+++ TODO.md
@@ -0,0 +1,16 @@
+TODO
+====
+
+
+- [x] link task (consume)
+- [x] unlink
+- [x] pull
+- [x] install with param
+- [x] install no param
+- [x] update
+- [x] uninstall
+- [ ] init
+- [ ] invalidate indices cache every 5 min 
+- [ ] cache lifetime in config
+- [ ] use cache on dev version (master)
+- [ ] manage deep deps on install
---
+++ test/config.test.js
@@ -0,0 +1,18 @@
+var assert = require('assert'),
+    config = require('../lib/config');
+
+describe('config', function() {
+    'use strict';
+
+    describe('config()', function() {
+        it('should be instance of function', function() {
+            assert(typeof config === 'function');
+        });
+
+        it('should return config', function() {
+            var c = config();
+            assert(typeof c === 'object');
+            assert.equal(c.cwd, process.cwd());
+        });
+    });
+});
